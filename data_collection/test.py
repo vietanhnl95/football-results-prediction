6de4e2c38fd4e0f0a64c36db3780067de2359a2c
Nguyễn Lê Việt Anh <33181603+vietanhnl95@users.noreply.github.com>
2021-10-10 20:40:38 +0700

    Delete test.py

--- data_collection/test.py
+++
@@ -1,9 +0,0 @@
-import utils
-import pandas as pd
-
-db_url = utils.read_config()['db_url']
-conn = utils.create_db_connection(db_url)
-
-sql_command = "SELECT COUNT(*) FROM football_api.fixtures WHERE league_id=39"
-result = pd.read_sql(sql_command, con=conn)
-print(result.loc[0, 'count'])
